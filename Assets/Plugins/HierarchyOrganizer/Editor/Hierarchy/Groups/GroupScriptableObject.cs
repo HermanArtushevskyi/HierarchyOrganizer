fbc9d4599a243abf70ec20c6d4dd536a6c6b391a
ArtushevskiiGerman <germanartushevskii@gmail.com>
2023-08-10 18:11:57 +0300

    Working

--- Assets/Plugins/HierarchyOrganizer/Editor/Hierarchy/Groups/GroupScriptableObject.cs
+++ Assets/Plugins/HierarchyOrganizer/Editor/Hierarchy/Groups/GroupScriptableObject.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using HierarchyOrganizer.Editor.Interfaces.Hierarchy;
+using TNRD;
 using UnityEngine;
 
 namespace HierarchyOrganizer.Editor.Hierarchy.Groups
@@ -9,8 +10,37 @@ namespace HierarchyOrganizer.Editor.Hierarchy.Groups
 	public class GroupScriptableObject : ScriptableObject, IGroup
 	{
 		[SerializeField] private string _name;
-		[SerializeField] private ICondition[] _conditions;
-		[SerializeField] private IRestructure[] _restructures;
+
+		[SerializeField] private SerializableInterface<ICondition>[] _serializableConditions;
+		[SerializeField] private SerializableInterface<IRestructure>[] _serializableRestructures;
+
+		private ICondition[] _conditions
+		{
+			get
+			{
+				ICondition[] arr = new ICondition[_serializableConditions.Length];
+				for (int i = 0; i < _serializableConditions.Length; i++)
+				{
+					arr[i] = _serializableConditions[i].Value;
+				}
+
+				return arr;
+			}
+		}
+
+		private IRestructure[] _restructures
+		{
+			get
+			{
+				IRestructure[] arr = new IRestructure[_serializableRestructures.Length];
+				for (int i = 0; i < _serializableRestructures.Length; i++)
+				{
+					arr[i] = _serializableRestructures[i].Value;
+				}
+
+				return arr;
+			}
+		}
 		
 		public string Name => _name;
 
