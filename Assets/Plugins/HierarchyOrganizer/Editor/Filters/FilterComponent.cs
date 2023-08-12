a7f26ce9d71ee37124dcfb0cc644581a64be58f8
ArtushevskiiGerman <germanartushevskii@gmail.com>
2023-08-12 17:45:31 +0300

    Working

--- Assets/Plugins/HierarchyOrganizer/Editor/Filters/FilterComponent.cs
+++ Assets/Plugins/HierarchyOrganizer/Editor/Filters/FilterComponent.cs
@@ -6,7 +6,7 @@ namespace HierarchyOrganizer.Editor.Filters
     public sealed class FilterComponent : FilterBase
     {
         private readonly Predicate<string> _componentFilter;
- 
+
         private string _value;
 
         public FilterComponent(string value = null, Mode mode = Mode.Is)
@@ -15,9 +15,6 @@ namespace HierarchyOrganizer.Editor.Filters
 
             switch (mode)
             {
-                case Mode.Is:
-                    _componentFilter = IsPredicate;
-                    break;
                 case Mode.Contains:
                     _componentFilter = ContainsPredicate;
                     break;
@@ -28,38 +25,30 @@ namespace HierarchyOrganizer.Editor.Filters
 
             Filter = PredicateFunc;
         }
-        
-
-  
-   
 
         public enum Mode
         {
-            Is,
             Contains,
             Exclude
         }
 
-        private bool PredicateFunc(GameObject go)
+        private bool IncludePredicate(GameObject go)
         {
             Component[] components = go.GetComponents<Component>();
+
             foreach (Component component in components)
             {
-                if (_componentFilter.Invoke(component.GetType().Name))
-                {
+                if (component.GetType().Name == _value)
                     return true;
-                }
             }
+
             return false;
         }
 
-
-        private bool IsPredicate(Component component) => _componentFilter.Invoke(component.GetType().Name);
-
-        private bool ContainsPredicate(Component component) => component.GetType().Name.Contains(_value);
-
-
-        private bool ExcludePredicate(Component component) => !_componentFilter.Invoke(component.GetType().Name);
+        private bool ExcludePredicate(GameObject go)
+        {
+            return !IncludePredicate(go);
+        }
 
     }
 }
