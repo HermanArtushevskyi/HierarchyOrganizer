4d09137f1d8c9ef23bbd08154fe74023e4c6e758
Wuljack <wuljack.2017@gmail.com>
2023-08-12 05:42:33 +0200

    .

---
+++ Assets/Plugins/HierarchyOrganizer/Editor/Filters/FilterComponent.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using UnityEngine;
+
+namespace HierarchyOrganizer.Editor.Filters
+{
+    public sealed class FilterComponent : FilterBase
+    {
+        private readonly Predicate<string> _componentFilter;
+
+        private string _value;
+
+        public FilterComponent(string value = null, Mode mode = Mode.Is)
+        {
+            _value = value;
+
+            switch (mode)
+            {
+                case Mode.Is:
+                    _componentFilter = IsPredicate;
+                    break;
+                case Mode.Contains:
+                    _componentFilter = ContainsPredicate;
+                    break;
+                case Mode.Exclude:
+                    _componentFilter = ExcludePredicate;
+                    break;
+            }
+
+            Filter = PredicateFunc;
+        }
+
+
+
+        public enum Mode
+        {
+            Is,
+            Contains,
+            Exclude
+        }
+
+        private bool PredicateFunc(GameObject go) => _componentFilter.Invoke(go.tag);
+
+        private bool IsPredicate(string componentName) => componentName == _value;
+
+        private bool ContainsPredicate(string componentName) => componentName.Contains(_value);
+
+        private bool ExcludePredicate(string componentName) => componentName != _value;
+    }
+}
---
+++ Assets/Plugins/HierarchyOrganizer/Editor/Filters/UXMLAdapters/Filters/ComponentFilterAdapter.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using HierarchyOrganizer.Editor.Interfaces.Filters;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace HierarchyOrganizer.Editor.Filters.UXMLAdapters
+{
+    public class ComponentFilterElementAdapter : ISceneFilterElementAdapter
+    {
+        private const string UXML_PATH = "Assets/Plugins/HierarchyOrganizer/Editor/Filters/UXML/Filters/ComponentFilterView.uxml";
+
+        private VisualElement _root;
+        private TemplateContainer _el;
+        private EnumField _modeField;
+        private TextField _textField;
+        private Button _deleteButton;
+
+        public event Action<ComponentFilterElementAdapter> OnDelete = null;
+
+        public void Init(VisualElement root)
+        {
+            _el = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>(UXML_PATH).Instantiate();
+
+            _root = root;
+            root.Add(_el);
+
+            _modeField = _el.Q<EnumField>();
+            _textField = _el.Q<TextField>();
+            _el.Q<Button>().clicked += Destroy;
+
+            _modeField.Init(FilterComponent.Mode.Is);
+            _textField.value = "";
+        }
+        
+
+
+        public ISceneFilter GetFilter() => new FilterComponent(_textField.value, (FilterComponent.Mode)_modeField.value);
+
+        public bool ValidateGameObject(GameObject go)
+        {
+            return new FilterComponent(_textField.value, (FilterComponent.Mode)_modeField.value).MeetsRequirements(go);
+        }
+
+        public void Destroy()
+        {
+            DestroyWithoutNotification();
+            OnDelete?.Invoke(this);
+        }
+
+        public void DestroyWithoutNotification() => _root.Remove(_el);
+    }
+}
