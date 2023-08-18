283cf0926768c7cd26053f7f375f2d496d5cf75e
ArtushevskiiGerman <germanartushevskii@gmail.com>
2023-08-18 22:51:15 +0300

    Working

---
+++ Assets/Plugins/HierarchyOrganizer/Editor/Hierarchy/Restructurers/ColorRestructure.cs
@@ -0,0 +1,23 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace HierarchyOrganizer.Editor.Hierarchy.Restructurers
+{
+	public class ColorRestructure : RestructureBase
+	{
+		private readonly Color _color;
+
+		public ColorRestructure(Color color)
+		{
+			_color = color;
+		}
+
+		public override void Do(GameObject obj)
+		{
+		}
+
+		public override void Undo(GameObject obj)
+		{
+		}
+	}
+}
