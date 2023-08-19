8cdd720550ee0231ad8294b8cb6c3668a6c7f2ce
ArtushevskiiGerman <germanartushevskii@gmail.com>
2023-08-20 02:47:18 +0300

    Added static status restructure

---
+++ Assets/Plugins/HierarchyOrganizer/Editor/Hierarchy/ScriptableObjectAdapters/Restructures/StaticStatusRestructureAdapter.cs
@@ -0,0 +1,15 @@
+﻿using HierarchyOrganizer.Editor.Hierarchy.Restructurers;
+using HierarchyOrganizer.Editor.Interfaces.Hierarchy;
+using HierarchyOrganizer.Editor.Interfaces.Hierarchy.Factories;
+using UnityEngine;
+
+namespace HierarchyOrganizer.Editor.Hierarchy.ScriptableObjectAdapters.Restructures
+{
+	public class StaticStatusRestructureAdapter : ScriptableObject, IRestructureFactory
+	{
+		[SerializeField] private bool Value;
+		[SerializeField] private StaticStatusRestructure.Mode Mode;
+		
+		public IRestructure Create() => new StaticStatusRestructure(Value, Mode);
+	}
+}
