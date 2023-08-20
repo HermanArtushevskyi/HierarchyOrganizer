4f2ef8cc21d38a4b21bf3d7b2566f2bedb4c599d
ArtushevskiiGerman <germanartushevskii@gmail.com>
2023-08-20 21:41:07 +0300

    Added async code for finding monobehs in project and component restructure

--- Assets/Plugins/HierarchyOrganizer/Editor/Hierarchy/ScriptableObjectAdapters/Restructures/ComponentRestructureAdapter.cs
+++ Assets/Plugins/HierarchyOrganizer/Editor/Hierarchy/ScriptableObjectAdapters/Restructures/ComponentRestructureAdapter.cs
@@ -1,17 +1,22 @@
-﻿using HierarchyOrganizer.Editor.Hierarchy.Restructurers;
+﻿using System;
+using HierarchyOrganizer.Editor.Common;
+using HierarchyOrganizer.Editor.Hierarchy.Restructurers;
 using HierarchyOrganizer.Editor.Interfaces.Hierarchy;
 using HierarchyOrganizer.Editor.Interfaces.Hierarchy.Factories;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 namespace HierarchyOrganizer.Editor.Hierarchy.ScriptableObjectAdapters.Restructures
 {
 	[CreateAssetMenu(fileName = "Component", menuName = "HierarchyOrganizer/Restructures/ComponentRestructure")]
 	public class ComponentRestructureAdapter : ScriptableObject, IRestructureFactory
 	{
-		[SerializeReference] private DropdownField _script;
+		[SerializeField] private MonoBehaviourReference _reference;
 		[SerializeField] private ComponentRestructure.Mode _mode;
 
-		public IRestructure Create() => new ComponentRestructure(_script.GetType(), _mode);
+		public IRestructure Create()
+		{ 
+			return	new ComponentRestructure(HierarchyProjectUtils.GetMonoBehaviourByName(
+											_reference.MonoBehaviourName), _mode);
+		}
 	}
 }
