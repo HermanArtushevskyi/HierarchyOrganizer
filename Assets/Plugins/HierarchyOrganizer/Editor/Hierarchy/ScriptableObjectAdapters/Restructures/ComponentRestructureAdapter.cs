0b19dcc3ec2fe510246d1805e6a5ea1cb4b3ddd2
ArtushevskiiGerman <germanartushevskii@gmail.com>
2023-08-20 03:36:27 +0300

    Working

---
+++ Assets/Plugins/HierarchyOrganizer/Editor/Hierarchy/ScriptableObjectAdapters/Restructures/ComponentRestructureAdapter.cs
@@ -0,0 +1,17 @@
+﻿using HierarchyOrganizer.Editor.Hierarchy.Restructurers;
+using HierarchyOrganizer.Editor.Interfaces.Hierarchy;
+using HierarchyOrganizer.Editor.Interfaces.Hierarchy.Factories;
+using UnityEditor;
+using UnityEngine;
+
+namespace HierarchyOrganizer.Editor.Hierarchy.ScriptableObjectAdapters.Restructures
+{
+	[CreateAssetMenu(fileName = "Component", menuName = "HierarchyOrganizer/Restructures/ComponentRestructure")]
+	public class ComponentRestructureAdapter : ScriptableObject, IRestructureFactory
+	{
+		[SerializeField] private MonoScript _script;
+		[SerializeField] private ComponentRestructure.Mode _mode;
+
+		public IRestructure Create() => new ComponentRestructure(_script.GetType(), _mode);
+	}
+}
