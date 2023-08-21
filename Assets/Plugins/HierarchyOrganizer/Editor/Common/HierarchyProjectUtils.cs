aba0b28481242df60cc20e10fd8cdc8f493adaf5
ArtushevskiiGerman <germanartushevskii@gmail.com>
2023-08-21 18:05:37 +0300

    Boosted HierarchyProjectUtils perfomance by adding caches

--- Assets/Plugins/HierarchyOrganizer/Editor/Common/HierarchyProjectUtils.cs
+++ Assets/Plugins/HierarchyOrganizer/Editor/Common/HierarchyProjectUtils.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Reflection;
 using System.Threading.Tasks;
 using UnityEngine;
@@ -8,8 +9,17 @@ namespace HierarchyOrganizer.Editor.Common
 {
 	public static class HierarchyProjectUtils
 	{
+		private static bool TypesDirty = true;
+		private static bool NamesDirty = true;
+
+		private static Type[] _typesCache = new Type[]{};
+		private static string[] _namesCache = new string[]{};
+		private static Dictionary<string, Type> _monobehsCache = new();
+
 		public static Type[] GetAllMonoBehavioursInProjectAsync()
 		{
+			if (!TypesDirty) return _typesCache;
+			
 			Assembly[] assemblies = AppDomain.CurrentDomain.GetAssemblies();
 
 			ConcurrentBag<Type> list = new ConcurrentBag<Type>();
@@ -26,11 +36,18 @@ namespace HierarchyOrganizer.Editor.Common
 					}
 				}
 			});
-			return list.ToArray();
+
+			var res = list.ToArray();
+
+			TypesDirty = false;
+			_typesCache = res;
+			return res;
 		}
 		
 		public static string[] GetAllMonoBehavioursNamesInProjectAsync()
 		{
+			if (!NamesDirty) return _namesCache;
+			
 			Assembly[] assemblies = AppDomain.CurrentDomain.GetAssemblies();
 
 			ConcurrentBag<string> list = new ConcurrentBag<string>();
@@ -47,11 +64,18 @@ namespace HierarchyOrganizer.Editor.Common
 					}
 				}
 			});
-			return list.ToArray();
+			
+			var res = list.ToArray();
+
+			NamesDirty = false;
+			_namesCache = res;
+			return res;
 		}
 
 		public static Type GetMonoBehaviourByName(string name)
 		{
+			if (_monobehsCache.ContainsKey(name)) return _monobehsCache[name];
+			
 			Assembly[] assemblies = AppDomain.CurrentDomain.GetAssemblies();
 
 			Type resType = null;
@@ -70,7 +94,9 @@ namespace HierarchyOrganizer.Editor.Common
 					}
 				}
 			});
-
+			
+			_monobehsCache.Add(name, resType);
+			
 			return resType;
 		}
 	}
