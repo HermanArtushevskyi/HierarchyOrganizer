0180e01fbd6844aeb9350de23a7f0cdce50b0b58
ArtushevskiiGerman <germanartushevskii@gmail.com>
2023-08-26 00:49:48 +0300

    Added EnabledCondition and EnabledRestructure

---
+++ Assets/Plugins/HierarchyOrganizer/Editor/Hierarchy/Conditions/EnabledCondition.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace HierarchyOrganizer.Editor.Hierarchy.Conditions
+{
+    public class EnabledCondition : ConditionBase
+    {
+        private bool _value;
+        
+        public EnabledCondition(bool value, Mode mode)
+        {
+            _value = value;
+
+            if (mode == Mode.Is) Condition = IsPredicate;
+        }
+
+        public enum Mode
+        {
+            Is,
+            
+        }
+
+        private bool IsPredicate(GameObject go) => go.activeInHierarchy == _value;
+    }
+}
---
+++ Assets/Plugins/HierarchyOrganizer/Editor/Hierarchy/Restructurers/EnabledRestructure.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using UnityEngine;
+
+namespace HierarchyOrganizer.Editor.Hierarchy.Restructurers
+{
+    public class EnabledRestructure : RestructureBase
+    {
+        private readonly bool _value;
+
+        private bool _oldValue;
+        
+        private Action<GameObject> _doAction;
+        private Action<GameObject> _undoAction;
+
+
+        public EnabledRestructure(bool value, Mode mode)
+        {
+            _value = value;
+            if (mode == Mode.Set)
+            {
+                _doAction = go =>
+                {
+                    _oldValue = go.activeInHierarchy;
+                    go.SetActive(_value);
+                };
+
+                _undoAction = go =>
+                {
+                    bool currentValue = go.activeInHierarchy;
+                    go.SetActive(_oldValue);
+                    _oldValue = currentValue;
+                };
+            }
+            
+            else if (mode == Mode.Switch)
+            {
+                _doAction = go =>
+                {
+                    go.SetActive(!go.activeInHierarchy);
+                };
+
+                _undoAction = go =>
+                {
+                    go.SetActive(!go.activeInHierarchy);
+                };
+            }
+        }
+
+        public enum Mode
+        {
+            Set,
+            Switch
+        }
+        
+        public override void Do(GameObject obj) => _doAction.Invoke(obj);
+
+        public override void Undo(GameObject obj) => _undoAction.Invoke(obj);
+    }
+}
