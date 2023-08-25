e9c906991a9d180904a4799832a3f9521e04f32e
ArtushevskiiGerman <germanartushevskii@gmail.com>
2023-08-26 00:18:38 +0300

    Added TagRestructure

---
+++ Assets/Plugins/HierarchyOrganizer/Editor/Hierarchy/ScriptableObjectAdapters/Restructures/TagRestructureAdapter.cs
@@ -0,0 +1,17 @@
+﻿using HierarchyOrganizer.Editor.Common;
+using HierarchyOrganizer.Editor.Hierarchy.Restructurers;
+using HierarchyOrganizer.Editor.Interfaces.Hierarchy;
+using HierarchyOrganizer.Editor.Interfaces.Hierarchy.Factories;
+using UnityEngine;
+
+namespace HierarchyOrganizer.Editor.Hierarchy.ScriptableObjectAdapters.Restructures
+{
+    [CreateAssetMenu(fileName = "TagRestructure", menuName = "HierarchyOrganizer/Restructures/Tag", order = 0)]
+    public class TagRestructureAdapter : ScriptableObject, IRestructureFactory
+    {
+        [SerializeField] private SerializedTagField _tag;
+        [SerializeField] private TagRestructure.Mode _mode;
+        
+        public IRestructure Create() => new TagRestructure(_tag.Value, _mode);
+    }
+}
